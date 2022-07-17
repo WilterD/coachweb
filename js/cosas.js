7f33f65117384a644f218ee3c406ea4c610a53f1
WilterD <wilterd18@gmail.com>
2022-07-17 12:24:37 -0400

    cambios de eswtetica

---
+++ js/cosas.js
@@ -0,0 +1,5 @@
+let anio = new Date().getFullYear();
+
+let miFooter = document.getElementById('miFooter');
+
+miFooter.innerHTML = `<p>Copyright Proempresarial &copy; ${anio} - Todos los derechos reservados</p>`;
